0171166d2f6b028898eef2591596d5f4b8c6a639
Vipul Sharma <sharmavips@google.com>
2022-10-27 22:47:39 +0530

    Added main , before_script and setup scripts

---
+++ files/setup.py
@@ -0,0 +1,19 @@
+import sys
+import json
+import base64
+from pymongo import MongoClient
+
+out = base64.b64decode(sys.argv[1])
+hosts=json.loads(out)
+dest=hosts['dest']
+
+client = MongoClient('0.0.0.0', 27017)
+db = client.admin
+helloOut = db.command("hello")
+primaryIP = helloOut['primary'].split(":")[0]
+
+client = MongoClient(primaryIP, 27017)
+db = client.admin
+replSetConfig=db.command("replSetGetConfig", 1)['config']
+members=replSetConfig['members']
+print(members)
