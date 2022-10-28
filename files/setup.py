f12847f1782334805b4bb15f905ff0da9dd83a41
Vipul Sharma <vipul.s.google@aux.gojek.com>
2022-10-28 03:02:12 +0000

    Update setup.py

--- files/setup.py
+++ files/setup.py
@@ -30,7 +30,7 @@ def addSecondary():
         replSetConfig['version'] += 1
         res = db.command('replSetReconfig', replSetConfig)
 
-    time.sleep(240)
+    time.sleep(180)
     replSetStatus=db.command('replSetGetStatus', 1)
     output={}
     for item in replSetStatus['members']:
