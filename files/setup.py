c5a9f043a5abae147638830af92cd4add98662cd
Vipul Sharma <vipul.s.google@aux.gojek.com>
2022-10-28 00:33:09 +0000

    Update setup.py

--- files/setup.py
+++ files/setup.py
@@ -28,7 +28,7 @@ for item in dest:
 replSetConfig['config']['members']=members
 replSetConfig['config']['version'] += 1
 
-res = db.comamnd('replSetReconfig', replSetConfig)
+res = db.command('replSetReconfig', replSetConfig)
 print(res)
 replSetConfig=db.command("replSetGetConfig", 1)
 print(replSetConfig)
