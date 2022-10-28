41d42308ffc8e18f28858b74eba33d10c040da9b
Vipul Sharma <sharmavips@google.com>
2022-10-28 07:19:07 +0530

    Added replica set status output

--- files/setup.py
+++ files/setup.py
@@ -1,33 +1,39 @@
 import sys
 import json
 import base64
+import time
 from pymongo import MongoClient
 
 out = base64.b64decode(sys.argv[1])
 hosts=json.loads(out)
 dest=hosts['dest']
 
-client = MongoClient('0.0.0.0', 27017)
-db = client.admin
-helloOut = db.command("hello")
-primaryIP = helloOut['primary'].split(":")[0]
+def addSecondary():
+    client = MongoClient('0.0.0.0', 27017)
+    db = client.admin
+    helloOut = db.command("hello")
+    primaryIP = helloOut['primary'].split(":")[0]
 
-client = MongoClient(primaryIP, 27017)
-db = client.admin
-replSetConfig=db.command("replSetGetConfig", 1)['config']
-members=replSetConfig['members']
+    client = MongoClient(primaryIP, 27017)
+    db = client.admin
+    replSetConfig=db.command("replSetGetConfig", 1)['config']
+    members=replSetConfig['members']
 
-for item in dest:
-    new={}
-    new['_id'] = members[-1]['_id']+1
-    new['host'] = item
-    new['hidden'] = False
-    new['priority'] = 0
-    members.append(new)
-    replSetConfig['members']=members
-    replSetConfig['version'] += 1
-    res = db.command('replSetReconfig', replSetConfig)
+    for item in dest:
+        new={}
+        new['_id'] = members[-1]['_id']+1
+        new['host'] = item
+        new['hidden'] = False
+        new['priority'] = 0
+        members.append(new)
+        replSetConfig['members']=members
+        replSetConfig['version'] += 1
+        res = db.command('replSetReconfig', replSetConfig)
 
-replSetStatus=db.command('replSetGetStatus', 1)
-print(replSetStatus['members'])
+    time.sleep(120)
+    replSetStatus=db.command('replSetGetStatus', 1)
+    return replSetStatus['members']
+
+if __name__ == "__main__":
+    addSecondary()
 
