1755d44a067b9ef52e24fce995499a34f50fbcf7
Vipul Sharma <sharmavips@google.com>
2022-10-28 09:07:09 +0530

    Added status python script

---
+++ files/status.py
@@ -0,0 +1,25 @@
+import sys
+import json
+from pymongo import MongoClient
+
+def status():
+    client = MongoClient('0.0.0.0', 27017)
+    db = client.admin
+    helloOut = db.command("hello")
+    primaryIP = helloOut['primary'].split(":")[0]
+
+    client = MongoClient(primaryIP, 27017)
+    db = client.admin
+    replSetStatus=db.command('replSetGetStatus', 1)
+    output={}
+    for item in replSetStatus['members']:
+        new={}
+        new['health']=item['health']
+        new['stateStr']=item['stateStr']
+        new['uptime']=item['uptime']
+        output[item['name']]=new
+    print(output)
+
+if __name__ == "__main__":
+    status()
+
