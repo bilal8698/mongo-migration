edf75a15dfb85b83c05e6cafad8142417cdbce9f
Vipul Sharma <sharmavips@google.com>
2022-11-03 14:42:22 +0530

    Added changes to support rollback/cutover stage

--- files/cutover.py
+++ files/cutover.py
@@ -1,3 +1,4 @@
+from pprint import pp
 import sys
 import json
 import base64
@@ -9,6 +10,8 @@ out = base64.b64decode(sys.argv[1])
 hosts=json.loads(out)
 dest=hosts['dest']
 toBePrimary = dest[0]
+saddr=socket.gethostbyaddr(toBePrimary)
+primary=''
 
 def cutover():
     client = MongoClient('0.0.0.0', 27017)
@@ -21,33 +24,55 @@ def cutover():
     replSetConfig=db.command("replSetGetConfig", 1)['config']
     members=replSetConfig['members']
 
-    saddr=socket.gethostbyaddr(toBePrimary)
-
     for item in members:
         if saddr[0] in item['host'] or ''.join(saddr[2]) in item['host']:
-            item['priority'] = 2
+            item['priority'] = priority() + 1
             break
 
     replSetConfig['members']=members
     replSetConfig['version'] += 1
     res = db.command('replSetReconfig', replSetConfig)
-    
-    time.sleep(180)
-
-    helloOut = db.command("hello")
-    primaryIP = helloOut['primary'].split(":")[0]
-
-    client = MongoClient(primaryIP, 27017)
-    db = client.admin
-    replSetStatus=db.command('replSetGetStatus', 1)
-    output={}
-    for item in replSetStatus['members']:
-        new={}
-        new['health']=item['health']
-        new['stateStr']=item['stateStr']
-        new['uptime']=item['uptime']
-        output[item['name']]=new
+    output=resetPriority()
     print(output)
+
+
+def priority():
+    client = MongoClient(primary, 27017)
+    db = client.admin
+    replSetConfig=db.command("replSetGetConfig", 1)['config']
+    members=replSetConfig['members']
+    for item in members:
+        if primary in item['host']:
+            return item['priority']
+
+def resetPriority():
+    time.sleep(180)
+    output={}
+    client = MongoClient(primary, 27017)
+    db = client.admin
+    helloOut = db.command("hello")
+    if toBePrimary ==  helloOut['primary'].split(":")[0]:
+        primary = helloOut['primary'].split(":")[0]
+        client = MongoClient(primary, 27017)
+        db = client.admin
+        replSetConfig=db.command("replSetGetConfig", 1)['config']
+        members=replSetConfig['members']
+        for item in members:
+            new={}
+            if saddr[0] in item['host'] or ''.join(saddr[2]) in item['host']:
+                item['priority'] = priority() - 1
+            new['health']=item['health']
+            new['stateStr']=item['stateStr']
+            new['uptime']=item['uptime']
+            output[item['name']]=new
+        replSetConfig['members']=members
+        replSetConfig['version'] += 1
+        res = db.command('replSetReconfig', replSetConfig)
+        return output
+    else:
+        resetPriority()
+
+    
 
 if __name__ == "__main__":
     cutover()
