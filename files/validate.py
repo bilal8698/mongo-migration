e642badabb7d778f3a1b28bbc5505d583b023441
Vipul Sharma <sharmavips@google.com>
2022-11-15 19:30:42 +0530

    Changed names of different mongo pipeline stages

--- files/validate.py
+++ files/validate.py
@@ -1,51 +1,30 @@
 import sys
 import json
-import base64
-import time
 from pymongo import MongoClient
 
-def validate():
-    dbsrc={}
-    dbdest={}
-    out = base64.b64decode(sys.argv[1])
-    hosts=json.loads(out)
-    dest=hosts['dest'][0]
-    src=hosts['src'][0]
+def status():
+    client = MongoClient('0.0.0.0', 27017)
+    db = client.admin
+    helloOut = db.command("hello")
+    primaryIP = helloOut['primary'].split(":")[0]
 
-    clientsrc = MongoClient(src, 27017)
-    clientdest = MongoClient(dest, 27017)
+    client = MongoClient(primaryIP, 27017)
+    db = client.admin
+    replSetStatus=db.command('replSetGetStatus', 1)
     output={}
-    output['Database Name']={ 'Source': 'Source MD5 Hash', 'Destination':'Destination MD5 Hash' }
-
-    for database in clientsrc.list_databases():
-            dbsrc[database['name']] = database
-    
-    for database in clientdest.list_databases():
-            dbdest[database['name']] = database
-    
-    if set(dbsrc.keys()) != set(dbdest.keys()):
-        print("Mismatch between Databases in Source and Destination Nodes - Data Validation FAILED")
-        return
-    else:
-        for db in set(dbsrc.keys()):
-            if dbsrc[db]['sizeOnDisk'] != dbdest[db]['sizeOnDisk']:
-                print("Mismatch between sizeOnDisk for Database "+db+" in Source "+src+"  and Destination Nodes "+dest+" - Data Validation FAILED")
-                return
-
-    for name in dbsrc.keys():
-        dbsrc=clientsrc[name]
-        dbdest=clientdest[name]
-        ressrc=dbsrc.command( "dbHash" , 1 )
-        resdest=dbdest.command( "dbHash" , 1 )
-        if ressrc['md5'] != resdest['md5']:
-            print("Database "+name+" md5 hash does not match for Source "+src+" and Destination "+dest+" Mongo Node - Data Validation FAILED")
-            return
+    for item in replSetStatus['members']:
+        new={}
+        if item['health'] == 0:
+            new['health']="DOWN"
         else:
-            output[name]={'Source':ressrc['md5'] , 'Destination':resdest['md5']}
-        
+            new['health']="UP"
+        new['stateStr']=item['stateStr']
+        new['uptime']=item['uptime']
+        if item['lastHeartbeatMessage'] != "":
+            new['lastHeartbeatMessage'] = item['lastHeartbeatMessage']
+        output[item['name']]=new
     print(output)
 
-
 if __name__ == "__main__":
-    validate()
+    status()
 
